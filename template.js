453349273a1be9b43a11354a3364ccf372801acb
DMBrar <dmvbrar@gmail.com>
2024-01-12 10:12:13 -0500

    edit to template

--- template.js
+++ template.js
@@ -2,5 +2,6 @@ var lastName = "Smith";
 var firstName = "John";
 var middleName = "Doe";
 
+//console.log(lastName + ", " + firstName + " " + middleName);
 console.log(`${lastName}, ${firstName} ${middleName}`);
 
